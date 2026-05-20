c540bb0cd518a357ecfb5459804626cb06accc55
wdgvideography-lgtm <wdg.videography@gmail.com>
2026-05-20 18:54:33 +0100

    Stub tRPC server import for frontend-only Vercel build

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,7 @@
 import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "../../../server/routers";
+import type { AnyRouter } from "@trpc/server";
+
+// Stub type for frontend-only build (server not bundled)
+type AppRouter = AnyRouter;
 
 export const trpc = createTRPCReact<AppRouter>();
