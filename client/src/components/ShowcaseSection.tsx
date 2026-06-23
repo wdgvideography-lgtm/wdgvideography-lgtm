4b9acf25a4880ebf1511d8f044ac4bc3a6f35553
wdgvideography-lgtm <wdg.videography@gmail.com>
2026-06-23 23:56:52 +0100

    Fix: replace react-router-dom with wouter for Link component

--- client/src/components/ShowcaseSection.tsx
+++ client/src/components/ShowcaseSection.tsx
@@ -5,7 +5,7 @@
 
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link } from "wouter";
 
 type Tab = "video" | "web" | "product";
 
